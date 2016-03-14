343fab360babeb78cb0e6519b2d4676383945be6
Kevin DeLoach <kdeloach@azavea.com>
2016-03-14 17:58:55 -0400

    Describe endpoints on landing page and add test form

--- hasher_service.py
+++ hasher_service.py
@@ -1,21 +1,33 @@
-from flask import Flask, request, jsonify
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
 import subprocess
+
+from flask import Flask, request, jsonify, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World (Flask)!"
 
-@app.route("/decrypt")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/decrypt')
 def decrypt():
     d = request.args.get('d', '')
     jsonp = request.args.get('jsonp', False)
     json = decrypt_result(d)
+
     if jsonp:
-        return "{0}({1});".format(jsonp, json)
+        return '{0}({1});'.format(jsonp, json)
     else:
         return jsonify(json)
+
 
 def decrypt_result(value):
     res = subprocess.Popen(
@@ -23,7 +35,11 @@ def decrypt_result(value):
         stdout=subprocess.PIPE
     )
     lines = [line.rstrip() for line in res.stdout]
-    return dict(original=str(value), decrypted=str(lines[0]))
+    return {
+        'original': str(value),
+        'decrypted': str(lines[0]),
+    }
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(port=5678)
